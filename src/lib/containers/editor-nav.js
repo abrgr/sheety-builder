9c8bb8fe435d78520c943fc7b1e6feffeae080e8
Adam Berger <adam.g.berger@gmail.com>
2018-04-08 16:14:03 -0400

    Better menu

--- src/lib/containers/editor-nav.js
+++ src/lib/containers/editor-nav.js
@@ -7,21 +7,28 @@ import BasicIcon from 'material-ui/svg-icons/action/list';
 import PresentationIcon from 'material-ui/svg-icons/action/view-quilt';
 import LogicIcon from 'material-ui/svg-icons/device/developer-mode';
 import PreviewIcon from 'material-ui/svg-icons/navigation/fullscreen';
+import Divider from 'material-ui/Divider';
 import { editorActions } from '../action-creators';
 import Nav from '../components/nav';
+import { editorRoutes, projectRoutes, appRoutes } from '../routes';
 
 class EditorNav extends Component {
   render() {
-    const { app, match, displayName, email, photoURL } = this.props;
+    const { app, project, appVersion, match, displayName, email, photoURL, isLoading } = this.props;
     const page = match && match.params.page;
     const orgPrefix = app.get('orgName')
                     ? (app.get('orgName') + ' > ')
                     : '';
 
+    const orgId = appVersion.get('orgId');
+    const projectId = appVersion.get('projectId');
+    const appId = appVersion.get('appId');
+    const versionId = appVersion.get('name');
+
     return (
       <Nav
         ref={nav => { this.nav = nav; }}
-        title={`${orgPrefix}${app.get('name')}`}
+        title={isLoading ? '' : `${orgPrefix}${project.get('name')} / ${app.get('name')} / ${appVersion.get('name')}`}
         displayName={displayName}
         email={email}
         photoURL={photoURL}
@@ -38,22 +45,26 @@ class EditorNav extends Component {
           <MenuItem
             key="home"
             insetChildren={true}
-            onClick={() => { this.props.history.push('/'); }}
+            onClick={this.onNavTo.bind(null, projectRoutes.list())}
             rightIcon={<HomeIcon />}>
             Home
           </MenuItem>,
+          <Divider
+            key="home-divider" />,
           <MenuItem
-            key="basic"
+            key="app"
             insetChildren={true}
-            onClick={this.onNavTo.bind(null, 'basic')}
+            onClick={this.onNavTo.bind(null, appRoutes.default(orgId, projectId, appId))}
             rightIcon={<BasicIcon />}
             checked={page === 'basic'}>
-            Basic setup
+            App Home
           </MenuItem>,
+          <Divider
+            key="app-divider" />,
           <MenuItem
             key="logic"
             insetChildren={true}
-            onClick={this.onNavTo.bind(null, 'logic')}
+            onClick={this.onNavTo.bind(null, editorRoutes.logicTab(orgId, projectId, appId, versionId))}
             rightIcon={<LogicIcon />}
             checked={page === 'logic'}>
             Logic editor
@@ -61,7 +72,7 @@ class EditorNav extends Component {
           <MenuItem
             key="presentation"
             insetChildren={true}
-            onClick={this.onNavTo.bind(null, 'presentation')}
+            onClick={this.onNavTo.bind(null, editorRoutes.presentationTab(orgId, projectId, appId, versionId))}
             rightIcon={<PresentationIcon />}
             checked={page === 'presentation'}>
             Presentation editor
@@ -103,6 +114,7 @@ class EditorNav extends Component {
 export default withRouter(
   connect(
     ({ auth, project, editor }) => ({
+      isLoading: project.get('isLoading') || editor.get('isLoading'),
       project: project.get('project'),
       app: editor.get('app'),
       appVersion: editor.get('appVersion'),
